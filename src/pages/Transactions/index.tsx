45a311470496cf6b7b58cce90a8327270ceef87b
kobe-leonel <leonel.farias@kobe.io>
2023-08-29 16:47:42 -0300

    add table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,40 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./style";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%"> Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+
+            <tr>
+              <td width="40%"> Comida</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 2.000,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
