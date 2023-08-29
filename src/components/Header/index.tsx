78ed956de34ddc4f81b0d302bdf5cc71f7c2fe17
kobe-leonel <leonel.farias@kobe.io>
2023-08-29 17:09:52 -0300

    add dialog modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,27 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import logo from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
