812ee281a102a28f267adac238c684734b342029
kobe-leonel <leonel.farias@kobe.io>
2023-08-30 11:19:19 -0300

    add provider

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  category: string;
+  price: number;
+  createdAt: string;
+}
+
+interface ITransactionContext {
+  transactions: Transaction[];
+}
+
+export const TransactionContext = createContext({} as ITransactionContext);
+
+interface ITransactionsProvider {
+  children: ReactNode;
+}
+export function TransactionsProvider({ children }: ITransactionsProvider) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+}
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -7,28 +7,11 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./style";
-
-interface ITransaction {
-  id: number;
-  description: string;
-  type: "income" | "outcome";
-  category: string;
-  price: number;
-  createdAt: string;
-}
+import { TransactionContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+  const { transactions } = useContext(TransactionContext);
 
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3333/transactions");
-    const data = await response.json();
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
   return (
     <div>
       <Header />
